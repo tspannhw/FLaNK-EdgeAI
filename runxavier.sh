ffbca6cbc1edacc6590ad679ea2498deace3a22f
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-12-23 20:40:13 -0500

    Update runxavier.sh

--- runxavier.sh
+++ runxavier.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+jetson_clocks
 DATE=$(date +"%Y-%m-%d_%H%M")
 python3 /home/nvidia/nvme/minifi-jetson-xavier/demoxavier.py --camera /dev/video0 --network googlenet /home/nvidia/nvme/images/$DATE.jpg
