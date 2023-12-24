3a6055a290958e7290c468da37539b5246ffc1a9
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-12-23 20:38:39 -0500

    Create runxavier.sh

---
+++ runxavier.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H%M")
+python3 /home/nvidia/nvme/minifi-jetson-xavier/demoxavier.py --camera /dev/video0 --network googlenet /home/nvidia/nvme/images/$DATE.jpg
